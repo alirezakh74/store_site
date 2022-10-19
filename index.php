26543dedbf9c418dbcb7f280a1cf87197b3a5640
Alireza Khodabande <alirezakhodabande74@gmail.com>
2022-10-19 13:09:40 -0700

    add first files

---
+++ index.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>home page</title>
+</head>
+
+<body>
+<?php
+echo "hello" . "<br>" . "world";
+?>
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>login page</title>
+</head>
+
+<body>
+<?php
+echo "hello" . "<br>" . "world";
+?>
+</body>
+</html>
