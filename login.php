e07e22434b9102c44be096350cdb2f7e0f1814b2
Alireza Khodabande <alirezakhodabande74@gmail.com>
2022-12-02 23:31:29 +0330

    check inputs, save users, login registered users

--- login.php
+++ login.php
@@ -3,14 +3,36 @@ $pageTitle = "ورود";
 include("inc/header.php");
 ?>
 
+<?php
+require_once("inc/functions.php");
+if(isset($_POST['email']) and isset($_POST['pass']))
+{
+	if(isRegisteredUser($_POST['email']))
+	{
+		if(checkPassUser($_POST['pass']))
+		{
+			header("Location: dashboard.php");
+		}
+		else
+		{
+			echo "رمز عبور اشتباه است";
+		}
+	}
+	else
+	{
+		echo "نام کاربری ثبت نشده است";
+	}
+}
+?>
+
 <div class="login_form">
-	<form action="login_process.php" method="post">
+	<form action="" method="post">
 		<h2>فرم ورود</h2>
 		<!-- <label>نام کاربری</label> -->
-		<input type="text" placeholder="ورود نام کاربری">
+		<input class="ltr" type="email" name="email" placeholder="ورود ایمیل(نام کاربری)" required>
 		<br>
 		<!-- <label>نام خانوادگی</label> -->
-		<input type="password" placeholder="ورود پسورد">
+		<input class="ltr" type="password" name="pass" placeholder="ورود پسورد" required>
 		<br>
 		<input type="submit" value="ورود">
 	</form>
---
+++ signupCheck.php
@@ -0,0 +1,90 @@
+<?php
+$pageTitle = "بررسی ثبت نام";
+
+echo "<head> <style> *{ text-align: center; } </style> </head>";
+
+include("inc/header.php");
+?>
+
+<?php
+
+require_once("inc/functions.php");
+
+$fname;
+$lname;
+$email;
+$pass;
+$rpass;
+
+function startSave()
+{
+    if ( isset($_POST['fname']) && isset($_POST['lname']) && isset($_POST['email']) && isset($_POST['pass']) && isset($_POST['rpass']) ) {
+
+        global $fname, $lname, $email, $pass, $rpass;
+        $fname = input_check($_POST["fname"]);
+        $lname = input_check($_POST["lname"]);
+        $email = input_check(strtolower($_POST["email"]));
+        $pass = input_check($_POST["pass"]);
+        $rpass = input_check($_POST["rpass"]);
+
+        echo "نام: " . $fname . "<br>";
+        echo "نام خانوادگی: " . $lname . "<br>";
+        echo "ایمیل: " . $email . "<br>";
+        echo "پسورد: " . $pass . "<br>";
+
+        save_user();
+    } else {
+        echo "ثبت نام مشکل دارد و انجام نشد";
+    }
+}
+
+if(isset($_POST['pass']) and isset($_POST["rpass"]) and ($_POST['pass'] == $_POST['rpass']))
+{
+    startSave();
+}
+else
+{
+    echo "رمز مجدد مطابقت ندارد";
+}
+
+?>
+
+<table class="user_table">
+
+    <tr>
+        <th>شماره</th>
+        <th>نام</th>
+        <th>نام خانوادگی</th>
+        <th>ایمیل</th>
+        <th>پسورد</th>
+    </tr>
+    <?php
+    $file = fopen('users.txt', 'r');
+    while (!feof($file)) {
+        $str = trim(fgets($file), "\n");
+        $myarray = explode(',', $str);
+
+        if($myarray[0] == "") // or ($myarray[0] == null)
+        {
+            break;
+        }
+        echo "<tr>";
+        static $c = 1;
+        echo "<td>" . $c . "</td>";
+        $c++;
+        foreach ($myarray as $value) {
+            echo "<td>";
+            echo $value;
+            echo "</td>";
+        }
+        echo "</tr>";
+    }
+
+    fclose($file);
+    ?>
+
+</table>
+
+<?php
+include("inc/footer.php");
+?>
