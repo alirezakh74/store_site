dd65d11ae6ba687fc17d7c20f685b6e82c825488
Alireza Khodabande <alirezakhodabande74@gmail.com>
2022-12-02 23:15:51 +0330

    faster effects

---
+++ app.js
@@ -0,0 +1,13 @@
+//var s = document.getElementById("top");
+//s.innerText = "Hellow worldlaslj";
+
+//const d = new Date();
+//$('#jsdate').text(d);
+
+$('.rightNav').hide().fadeIn(2000);
+$('.leftNav').hide().slideDown(1500);
+$('.page').hide().slideDown(5000);
+$('.user_welcome').hide().slideDown(1700);
+
+
+//document.getElementById('jsdate').innerHTML = new Date();
