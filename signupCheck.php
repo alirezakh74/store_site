c0cd47138ef5ad7aa6e5e83ffd4f4f5196fedb43
Alireza Khodabande <alirezakhodabande74@gmail.com>
2023-01-20 01:02:40 +0330

    remove header function
    header problems solved by javascript location.href

--- signupCheck.php
+++ signupCheck.php
@@ -8,7 +8,8 @@ include("inc/header.php");
 
 <?php
 
-require_once("inc/functions.php");
+include("inc/functions.php");
+include("inc/consts.php");
 
 $fname;
 $lname;
@@ -18,7 +19,7 @@ $rpass;
 
 function startSave()
 {
-    if ( isset($_POST['fname']) && isset($_POST['lname']) && isset($_POST['email']) && isset($_POST['pass']) && isset($_POST['rpass']) ) {
+    if (isset($_POST['fname']) && isset($_POST['lname']) && isset($_POST['email']) && isset($_POST['pass']) && isset($_POST['rpass'])) {
 
         global $fname, $lname, $email, $pass, $rpass;
         $fname = input_check($_POST["fname"]);
@@ -38,13 +39,12 @@ function startSave()
     }
 }
 
-if(isset($_POST['pass']) and isset($_POST["rpass"]) and ($_POST['pass'] == $_POST['rpass']))
-{
+if (isset($_POST['pass']) and isset($_POST["rpass"]) and ($_POST['pass'] == $_POST['rpass'])) {
     startSave();
-}
-else
-{
-    echo "<p style='text-align: center; color: red;'>رمز مجدد مطابقت ندارد</p>";
+} else {
+    if (isset($_POST['pass']) and isset($_POST['rpass'])) {
+        echo "<p style='text-align: center; color: red;'>رمز مجدد مطابقت ندارد</p>";
+    }
 }
 
 ?>
@@ -56,34 +56,99 @@ else
         <th>نام</th>
         <th>نام خانوادگی</th>
         <th>ایمیل</th>
-        <th>پسورد</th>
+        <th>رمز عبور</th>
+        <th>ویرایش</th>
+        <th>حذف</th>
     </tr>
-    <?php
-    $file = fopen('users.txt', 'r');
-    while (!feof($file)) {
-        $str = trim(fgets($file), "\n");
-        $myarray = explode(',', $str);
 
-        if($myarray[0] == "") // or ($myarray[0] == null)
-        {
-            break;
+    <?php
+
+    //get current page number
+    $current_page = getPageNumber();
+    // every page how many record will show
+    $records_per_page = MAX_NUM_ROW_TABLE;
+    // calculate first record of current page
+    $from_record = ($current_page - 1) * $records_per_page;
+
+    $q = "SELECT * FROM users";
+    $result = mysqli_query(connect_db(), $q);
+    // get total record
+    $total_records = mysqli_num_rows($result);
+    // calculate all pages
+    $total_pages = ceil($total_records / $records_per_page);
+
+    $q = "SELECT fname,lname,email,password FROM users LIMIT $records_per_page OFFSET $from_record";
+    $result = mysqli_query(connect_db(), $q);
+
+    if (($result != "") and (mysqli_num_rows($result) != 0)) {
+        while ($rows = mysqli_fetch_array($result)) {
+
+            // start of row
+            echo "<tr>";
+            static $c = 1;
+            echo "<td>" . $c . "</td>";
+            $c++;
+            foreach ($rows as $key => $value) {
+                if (!is_numeric($key)) {
+                    echo "<td>";
+                    echo $value;
+                    echo "</td>";
+                }
+            }
+            if (!isset($_GET['page'])) {
+                echo "<td><a href='#'><img src='icons/edit.png'></img></a></td>";
+                echo "<td><a href='signupCheck.php?email=" . $rows['email'] . "'><img src='icons/delete.png'></img></a></td>";
+            } else {
+                echo "<td><a href='#'><img src='icons/edit.png'></img></a></td>";
+                echo "<td><a href='signupCheck.php?" . "page=" . $_GET['page'] . "&" . "email=" . $rows['email'] . "'><img src='icons/delete.png'></img></a></td>";
+            }
+
+            // end of row
+            echo "</tr>";
+
+            if (isset($_GET['email'])) {
+                $em = $_GET['email'];
+                $q = "DELETE FROM users WHERE email = '$em'";
+                mysqli_query(connect_db(), $q);
+                //echo "<meta http-equiv='refresh' content='0'>";
+                //header("Location: signupCheck.php?page=$current_page");
+                //exit();
+                echo("<script>location.href = " . "'signupCheck.php?page=" . "$current_page';</script>");
+            }
+
+            mysqli_close(connect_db());
         }
-        echo "<tr>";
-        static $c = 1;
-        echo "<td>" . $c . "</td>";
-        $c++;
-        foreach ($myarray as $value) {
-            echo "<td>";
-            echo $value;
-            echo "</td>";
-        }
-        echo "</tr>";
+    }
+    else if($result == "")
+    {
+        echo "<h3 style='color: red;'>هیچ کاربری وجود ندارد</h3>";
     }
 
-    fclose($file);
+    if($total_pages < $current_page){
+        //header("Location: signupCheck.php?page=$total_pages");
+        //exit();
+        echo("<script>location.href = " . "'signupCheck.php?page=" . "$total_pages';</script>");
+    }
     ?>
 
 </table>
+
+<div class="pagination">
+    <p>
+        <?php
+        $pagelink = "";
+        for ($i = 1; $i <= $total_pages; $i++) {
+            if ($i == $current_page) {
+                $pagelink .= "<a class='active' href='signupCheck.php?page=$i'>[صفحه $i]</a>";
+            } else {
+                $pagelink .= "<a href='signupCheck.php?page=$i'>صفحه $i</a>";
+            }
+        }
+
+        echo $pagelink;
+        ?>
+    </p>
+</div>
 
 <?php
 include("inc/footer.php");
